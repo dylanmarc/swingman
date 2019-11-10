974672accd4f1dc51e9c2da3309f3bc26375bf9c
dylanmarc <diloubilou@gmail.com>
2019-11-11 12:12:18 +1300

    started adding debug sliders, commented out item spawn

--- swingman.js
+++ swingman.js
@@ -74,7 +74,7 @@ function draw(){
 
   drawPipe(pipe);
 
-  spawnItem(item);
+  //spawnItem(item);
 
   setVelocity(man);
   drawMan(man);
@@ -204,4 +204,29 @@ function keyPressed() {
   }else if (keyCode == 87){
     man2.y_vel = -man2.jumpVel;
   }
+}
+
+//Debug setup
+var debugCheckbox;
+var hourSlider;
+var minSlider;
+var secSlider;
+var millisSlider;
+var alarmSlider;
+
+function debug_setup() {
+  debugCheckbox = createCheckbox('', false);
+  debugCheckbox.parent("checkboxDebug")
+  hourSlider = createSlider(0, 23, 12);
+  hourSlider.parent("sliderHours")
+  minSlider = createSlider(0, 59, 0);
+  minSlider.parent("sliderMinutes")
+  secSlider = createSlider(0, 59, 0);
+  secSlider.parent("sliderSeconds")
+  millisSlider = createSlider(0, 999, 0);
+  millisSlider.parent("sliderMillis")
+  alarmCheckbox = createCheckbox('', false);
+  alarmCheckbox.parent("checkboxAlarm")
+  alarmSlider = createSlider(0, 60, 0);
+  alarmSlider.parent("sliderAlarm")
 }
